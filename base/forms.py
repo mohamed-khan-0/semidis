5e5f0ef4bd8f98250de1c76ee9110ae01f6958eb
mohamed-khan-0 <khanmoh23@outlook.com>
2024-10-08 10:32:50 +0300

    login form

--- base/forms.py
+++ base/forms.py
@@ -1,8 +1,13 @@
+from django import forms
 from django.forms import ModelForm
-from .models import Room
+from .models import Room, Topic
 
 
 class RoomForm(ModelForm):
+    topic = forms.ModelChoiceField(queryset=Topic.objects.all(), widget=forms.Select(attrs={'class': 'form-control'}))
+    name = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    description = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control'}))
     class Meta:
         model = Room
         fields = ['topic', 'name', 'description']
+
